f9f1908d8ec407aab9de369c99277d05731ec963
Priyansh Jain <48293372+ErR0rpj@users.noreply.github.com>
2021-12-23 14:30:15 +0530

    Basic linear optimization with scip

--- linear_programming_basic2.cc
+++ linear_programming_basic2.cc
@@ -4,27 +4,38 @@ namespace operations_research
 {
 	void BasicLinearSolution()
 	{
-		//This example uses GLOP which is made by google for linear programming solutions.
-		std::unique_ptr<MPSolver> solver(MPSolver::CreateSolver("GLOP"));
+		//This example uses SCIP, which is not made by GOOGLE and is used to solve for integer values.
+		std::unique_ptr<MPSolver> solver(MPSolver::CreateSolver("SCIP"));
 
 		//Creating variable x and y;
-		MPVariable* const x = solver->MakeNumVar(0, 1, "x");
-		MPVariable* const y = solver->MakeNumVar(0, 2, "y");
+		MPVariable* const x = solver->MakeNumVar(0, INFINITY, "x");
+		MPVariable* const y = solver->MakeNumVar(0, INFINITY, "y");
 
 		LOG(INFO) << "Number of variables = " << solver->NumVariables();
 
-		//Creates a linear constraint constraint, x + y <= 2.
-		MPConstraint* const ct = solver->MakeRowConstraint(0, 2, "ct");
+		//Creates a linear constraint constraint, x + 2y <= 14.
+		MPConstraint* const ct = solver->MakeRowConstraint(0, 14);
 		//Sets the coefficient of x and y in constraint
 		ct->SetCoefficient(x, 1);
-		ct->SetCoefficient(y, 1);
+		ct->SetCoefficient(y, 2);
+
+		//Creates another constraint, 3x-y >= 0
+		MPConstraint* const ct2 = solver->MakeRowConstraint(0, INFINITY);
+		ct2->SetCoefficient(x, 3);
+		ct2->SetCoefficient(y, -1);
+
+		//Creates another constraint, x-y <= 2
+		MPConstraint* const ct3 = solver->MakeRowConstraint(-INFINITY, 2);
+		ct3->SetCoefficient(x, 1);
+		ct3->SetCoefficient(y, -1);
+
 
 		LOG(INFO) << "Number of constraints: " << solver->NumConstraints();
 
 		// Creates the main function which needs to be maximized
 		MPObjective* const objective = solver->MutableObjective();
 		objective->SetCoefficient(x, 3);
-		objective->SetCoefficient(y, 1);
+		objective->SetCoefficient(y, 4);
 		objective->SetMaximization();
 
 		solver->Solve();
